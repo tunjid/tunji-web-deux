fdeeb59b59825aca3ebb0eb18275f1af1b4f04c2
Adetunji Dahunsi <tjdah100@gmail.com>
2021-04-29 15:16:17 -0400

    Change router matching

--- client/src/routes.js
+++ client/src/routes.js
@@ -13,18 +13,18 @@ const routes = () => (
         <Switch>
             <Route exact path='/' component={Home}/>
             <Route exact path='/sign-in' component={SignIn}/>
-            <Route exact path={`/${ArchiveKind.Articles}`} component={ArchiveList}/>
-            <Route exact path={`/${ArchiveKind.Projects}`} component={ArchiveList}/>
-            <Route exact path={`/${ArchiveKind.Talks}`} component={ArchiveList}/>
             <Route exact path={`/${ArchiveKind.Articles}/create`} component={ArchiveCreate}/>
             <Route exact path={`/${ArchiveKind.Projects}/create`} component={ArchiveCreate}/>
             <Route exact path={`/${ArchiveKind.Talks}/create`} component={ArchiveCreate}/>
-            <Route path={`/${ArchiveKind.Articles}/:archiveId/edit`} component={ArchiveEdit}/>
-            <Route path={`/${ArchiveKind.Projects}/:archiveId/edit`} component={ArchiveEdit}/>
-            <Route path={`/${ArchiveKind.Talks}/:archiveId/edit`} component={ArchiveEdit}/>
-            <Route path={`/${ArchiveKind.Articles}/:archiveId`} component={ArchiveDetail}/>
-            <Route path={`/${ArchiveKind.Projects}/:archiveId`} component={ArchiveDetail}/>
-            <Route path={`/${ArchiveKind.Talks}/:archiveId`} component={ArchiveDetail}/>
+            <Route exact path={`/${ArchiveKind.Articles}/:archiveId/edit`} component={ArchiveEdit}/>
+            <Route exact path={`/${ArchiveKind.Projects}/:archiveId/edit`} component={ArchiveEdit}/>
+            <Route exact path={`/${ArchiveKind.Talks}/:archiveId/edit`} component={ArchiveEdit}/>
+            <Route exact path={`/${ArchiveKind.Articles}/:archiveId`} component={ArchiveDetail}/>
+            <Route exact path={`/${ArchiveKind.Projects}/:archiveId`} component={ArchiveDetail}/>
+            <Route exact path={`/${ArchiveKind.Talks}/:archiveId`} component={ArchiveDetail}/>
+            <Route path={`/${ArchiveKind.Articles}`} component={ArchiveList}/>
+            <Route path={`/${ArchiveKind.Projects}`} component={ArchiveList}/>
+            <Route path={`/${ArchiveKind.Talks}`} component={ArchiveList}/>
         </Switch>
     </ScrollToTop>
 )
