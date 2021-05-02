a925344141c1ba75a728d16f476566d9a35e0ce9
Adetunji Dahunsi <tjdah100@gmail.com>
2021-05-02 11:32:23 -0400

    Minor clean up

--- client/src/components/archive/ArchiveDetail.tsx
+++ client/src/components/archive/ArchiveDetail.tsx
@@ -141,15 +141,15 @@ const ArchiveDetail = () => {
                 remarkPlugins={[gfm]}
                 children={archive?.body || ''}
                 components={{
-                    img: ({node, ...props}) => (<img{...props} style={{maxWidth: '10vw'}}/>),
-                    p: ({node, ...props}) => (<p{...props} style={{fontSize: '150%'}}/>),
-                    li: ({node, ...props}) => (<p{...props} style={{fontSize: '150%'}}/>),
-                    h1: ({node, ...props}) => (<h1{...props} style={{fontSize: '150%'}}/>),
-                    h2: ({node, ...props}) => (<h2{...props} style={{fontSize: '150%'}}/>),
-                    h3: ({node, ...props}) => (<h3{...props} style={{fontSize: '150%'}}/>),
-                    h4: ({node, ...props}) => (<h4{...props} style={{fontSize: '150%'}}/>),
-                    h5: ({node, ...props}) => (<h5{...props} style={{fontSize: '150%'}}/>),
-                    h6: ({node, ...props}) => (<h6{...props} style={{fontSize: '150%'}}/>),
+                    img: ({node, ...props}) => <img{...props} style={{maxWidth: '10vw'}}/>,
+                    p: ({node, ...props}) => <p{...props} style={{fontSize: '150%'}}/>,
+                    li: ({node, ...props}) => <p{...props} style={{fontSize: '150%'}}/>,
+                    h1: ({node, ...props}) => <h1{...props} style={{fontSize: '150%'}}/>,
+                    h2: ({node, ...props}) => <h2{...props} style={{fontSize: '150%'}}/>,
+                    h3: ({node, ...props}) => <h3{...props} style={{fontSize: '150%'}}/>,
+                    h4: ({node, ...props}) => <h4{...props} style={{fontSize: '150%'}}/>,
+                    h5: ({node, ...props}) => <h5{...props} style={{fontSize: '150%'}}/>,
+                    h6: ({node, ...props}) => <h6{...props} style={{fontSize: '150%'}}/>,
                 }}
             />
 
