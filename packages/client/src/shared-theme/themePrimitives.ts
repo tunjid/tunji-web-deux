4ca3ffa920247ef18f7e1639dbd17468311797c4
Adetunji Dahunsi <tjdah100@gmail.com>
2025-04-06 15:15:38 -0700

    Update theming of alerts

--- packages/client/src/shared-theme/themePrimitives.ts
+++ packages/client/src/shared-theme/themePrimitives.ts
@@ -247,27 +247,6 @@ export const colorSchemes = {
         dark: brand[700],
         contrastText: brand[50],
       },
-      info: {
-        light: brand[100],
-        main: brand[300],
-        dark: brand[600],
-        contrastText: gray[50],
-      },
-      warning: {
-        light: orange[300],
-        main: orange[400],
-        dark: orange[800],
-      },
-      error: {
-        light: red[300],
-        main: red[400],
-        dark: red[800],
-      },
-      success: {
-        light: green[300],
-        main: green[400],
-        dark: green[800],
-      },
       grey: {
         ...gray,
       },
